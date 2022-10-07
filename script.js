ebb0dfaab94aeaaade8b04533b5f607bdf421c1d
Rishi <romitdasgupta3@gmail.com>
2022-10-07 13:00:08 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,41 @@
+let weather={
+    "apiKey":"32c8de74de4e8a90b8b0bc310a424ccb",
+    fetchweather: function(city){
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=metric&appid=32c8de74de4e8a90b8b0bc310a424ccb"
+        )
+        .then((response) => response.json())
+        .then((data) => this.displayweather(data));
+    },
+    displayweather: function(data){
+        const { name } = data;
+        const { icon , description } = data.weather[0];
+        const { temp , humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name,icon,description,temp,humidity,speed)
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°C";
+        document.querySelector(".humidity").innerText = "Humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "Wind speed: " + speed + "kmph";
+        document.querySelector(".weather").classList.remove ("loading");
+        document.body.style.backgroundImage = "url('https://source.unsplash.com/1600x900/?" + name + "')" 
+        
+    },
+    search: function () {
+        this.fetchweather(document.querySelector(".search-bar").value)
+    },
+};  
+
+document.querySelector(".search button").addEventListener("click",function () {
+    weather.search();
+});
+
+document.querySelector(".search-bar").addEventListener("keyup",function(event){
+    if(event.key == "Enter"){
+        weather.search();
+    }
+});
+
+weather.fetchweather("Delhi");
